9f5a556ba01e8d26903d3e876f1440d6c85c3e62
Jean-Baptiste Blanc <jb@mango.tools>
2016-03-01 17:30:15 +0000

    initial commit

---
+++ src/android/Discovery.java
@@ -0,0 +1,127 @@
+package tools.mango.homfi.cordova;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.PluginResult;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.util.HashMap;
+
+public class Discovery extends CordovaPlugin {
+
+    HashMap<String, MulticastSocket> sockets;
+    HashMap<String, SocketListener> listeners;
+
+    public Discovery() {
+        sockets = new HashMap<String, MulticastSocket>();
+        listeners = new HashMap<String, SocketListener>();
+    }
+
+    @Override
+    public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
+        
+        final String instanceId = data.getString(0);
+        MulticastSocket socket = sockets.get(instanceId);
+
+        if (action.equals("init")) {
+            if (socket == null) {
+                try {
+                    String host = data.getString(1);
+                    int port = data.getInt(2);
+
+                    socket = new MulticastSocket(port);
+                    socket.setTimeToLive(10); // Local network
+                    socket.joinGroup(InetAddress.getByName(host)); // Tell the OS to listen for messages on the specified host and treat them as if they were meant for this host
+                    Boolean disableLoopback = false;
+                    socket.setLoopbackMode(disableLoopback);
+                    
+                    sockets.put(instanceId, socket);
+                    callbackContext.success();
+                } catch (Exception e) {
+                    callbackContext.error(e.toString());
+                }
+            }
+        } else if (action.equals("listen")) {
+            try {
+                // Set up listener
+                SocketListener listener = new SocketListener(socket, callbackContext);
+                listeners.put(instanceId, listener);
+                listener.start();
+                // Don't run callbackContext.success() since that 'closes' the callbackcontext
+                // Such that is prevents us to send further .success(PluginResult.Status.OK) messages when the listener receives a message
+            } catch (Exception e) {
+                callbackContext.error(e.toString());
+            }
+        }  else if (action.equals("send")) {
+            String message = data.getString(1);
+            String host = data.getString(2);
+            int port = data.getInt(3);
+
+            try {
+                byte[] bytes = message.getBytes("UTF-8");
+                DatagramPacket packet = new DatagramPacket(bytes, bytes.length, InetAddress.getByName(host), port);
+                socket.send(packet);
+                callbackContext.success(InetAddress.getByName(host).getHostAddress() + ":" + message);
+            } catch (IOException ioe) {
+                callbackContext.error("IOException: " + ioe.toString());
+            } catch (Exception e) {
+                callbackContext.error("Exception: " + e.toString());
+            }
+        } else if (action.equals("close")) {
+            if (socket != null) {
+                socket.close();
+                sockets.remove(instanceId);
+                SocketListener listener = listeners.get(instanceId);
+                if (listener != null) {
+                    listener.interrupt();
+                    listeners.remove(instanceId);
+                }
+            }
+            callbackContext.success();
+        } else {
+            return false; // 'MethodNotFound'
+        }
+        return true;
+    }
+
+    private class SocketListener extends Thread {
+
+        MulticastSocket socket;
+        PluginResult result;
+        CallbackContext callbackContext;
+
+        public SocketListener(MulticastSocket socket, CallbackContext callbackContextParam) {
+            this.socket = socket;
+            callbackContext = callbackContextParam;
+        }
+
+        public void run() {
+            byte[] data = new byte[2048];
+            DatagramPacket packet = new DatagramPacket(data, data.length);
+            while (true) {
+                try {
+                    this.socket.receive(packet);
+                    String msg = new String(data, 0, packet.getLength(), "UTF-8")
+                                    .replace("'", "\'")
+                                    .replace("\r", "\\r")
+                                    .replace("\n", "\\n");
+
+                    result = new PluginResult(PluginResult.Status.OK, msg);
+                    result.setKeepCallback(true); // Allow for additional callbacks
+                    callbackContext.sendPluginResult(result);
+
+                } catch (Exception e) {
+                    return;
+                }
+            }
+        }
+    }
+
+}
---
+++ www/discovery.js
@@ -0,0 +1,101 @@
+var exec = cordova.require('cordova/exec');
+
+var MULTICAST_HOST = "224.0.0.236";
+var MULTICAST_PORT = 60555;
+
+module.exports = function() {
+
+	var instanceId = guid();
+
+	var events = {};
+	// =====
+	// Set up UDP Multicast connection
+	// =====
+
+	function initCallbackSuccess() {
+		queryAnnouncement();
+	}
+	function initCallbackFail(err) {
+		console.log(err)
+	}
+	exec(initCallbackSuccess, initCallbackFail, 'Discovery', 'init', [ instanceId, MULTICAST_HOST, MULTICAST_PORT ]);
+
+
+	function messageCallback(buffer) {
+		try {
+			var message = JSON.parse(buffer);
+
+			if(message.event ==='announce'){
+				if (events["announce"]) {
+					for(var callbackId in events["announce"]) {
+						var callback = events["announce"][callbackId];
+						callback(message);
+					}
+				}
+			}
+		} catch(e) {
+			// ignore...
+		}
+	}
+	exec(messageCallback, null, 'Discovery', 'listen', [ instanceId ]);
+
+	// =====
+	// Exported functions
+	// =====
+
+
+	exports.queryAnnouncement = function() {
+		queryAnnouncement();
+	}
+
+	exports.on = function(eventName, callback) {
+		if(!events[eventName]) {
+			events[eventName] = {};
+		}
+		var callbackId = guid();
+		events[eventName][callbackId] = callback;
+		return callbackId;
+	}
+
+	exports.off = function(eventName, callbackId) {
+		if(!events[eventName]) {
+			return false;
+		}
+		delete events[eventName][callbackId];
+		return true;
+	}
+
+
+
+	// =====
+	// Helper functions
+	// =====
+
+	function generalSuccessCallback(success) {
+		//console.log(success)
+	}
+
+	function generalFailCallback(error) {
+		//console.log(error)
+	}
+
+
+
+	function queryAnnouncement() {
+		var message = JSON.stringify({ event: "query"});
+		exec(generalSuccessCallback, generalFailCallback, 'Discovery', 'send', [ instanceId, message, MULTICAST_HOST, MULTICAST_PORT ]);
+	}
+
+	function guid() {
+		function s4() {
+			return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+		}
+		return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+	}
+
+	// =====
+	// Export
+	// =====
+
+	return exports;
+}
